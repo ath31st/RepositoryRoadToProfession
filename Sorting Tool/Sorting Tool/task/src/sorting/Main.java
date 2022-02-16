8e42c04956b3955fb4b4aeb134924da6b89023e1
Vladimir Olennikov <ath31st@yandex.ru>
2022-02-16 22:30:02 +0300

    solved 1/6 stage of project Sorting tool

--- Sorting Tool/Sorting Tool/task/src/sorting/Main.java
+++ Sorting Tool/Sorting Tool/task/src/sorting/Main.java
@@ -5,10 +5,15 @@ import java.util.*;
 public class Main {
     public static void main(final String[] args) {
         Scanner scanner = new Scanner(System.in);
+        List<Integer> list = new ArrayList<>();
 
         while (scanner.hasNextLong()) {
-            long number = scanner.nextLong();
-            // write your code here
+            list.add(scanner.nextInt());
         }
+        int total = list.size();
+        int i = list.stream().max(Integer::compare).get();
+        long count = list.stream().filter(integer -> integer == i).count();
+        System.out.printf("Total numbers: %d.\n", total);
+        System.out.printf("The greatest number: %d (%d time(s)).", i, count);
     }
 }
