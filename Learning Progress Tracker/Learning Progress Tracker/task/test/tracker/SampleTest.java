fd7190a7b7c4ce5ccf0fed8be1db4eb91ec51ba7
Vladimir Olennikov <ath31st@yandex.ru>
2022-03-22 19:09:37 +0300

    Start project Learning Progress Tracker, 1/5 stage solved

---
+++ Learning Progress Tracker/Learning Progress Tracker/task/test/tracker/SampleTest.java
@@ -0,0 +1,13 @@
+package tracker;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SampleTest {
+
+    @Test
+    void test() {
+        assertEquals(5, 2 + 3);
+    }
+}
