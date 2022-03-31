94fc5bd4123b90e28acc71108281ddf23d36cefc
Vladimir Olennikov <ath31st@yandex.ru>
2022-03-31 15:54:08 +0300

    project Web Quiz Engine, 2/6 stage solved

--- Web Quiz Engine/Web Quiz Engine/task/src/engine/Answer.java
+++ Web Quiz Engine/Web Quiz Engine/task/src/engine/Answer.java
@@ -1,2 +1,28 @@
-package engine;public class Answer {
+package engine;
+
+
+public class Answer {
+    private boolean success;
+    private String feedback;
+
+    public Answer(boolean success, String feedback) {
+        this.success = success;
+        this.feedback = feedback;
+    }
+
+    public boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getFeedback() {
+        return feedback;
+    }
+
+    public void setFeedback(String feedback) {
+        this.feedback = feedback;
+    }
 }
--- Web Quiz Engine/Web Quiz Engine/task/src/engine/Quiz.java
+++ Web Quiz Engine/Web Quiz Engine/task/src/engine/Quiz.java
@@ -1,2 +1,67 @@
-package engine;public class Quiz {
+package engine;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class Quiz {
+
+    private int id;
+    private String title;
+    private String text;
+    private List<String> options = new CopyOnWriteArrayList<>();
+    private int answer;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public List<String> getOptions() {
+        return options;
+    }
+
+    public void setOptions(List<String> options) {
+        this.options = options;
+    }
+
+    @JsonIgnore
+    public int getAnswer() {
+        return answer;
+    }
+
+    @JsonProperty
+    public void setAnswer(int answer) {
+        this.answer = answer;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public static void setIdAfterCreateList(List<Quiz> quizzes) {
+        int count = 1;
+        for (Quiz quiz : quizzes) {
+            quiz.setId(count);
+            count++;
+        }
+    }
 }
