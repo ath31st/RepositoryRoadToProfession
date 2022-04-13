d6f6441f7e3b09e3fe116ef7101e5e54b3b447da
Vladimir Olennikov <ath31st@yandex.ru>
2022-04-13 16:03:04 +0300

    project Code Sharing Platform, processing stage 3.1

--- Code Sharing Platform/Code Sharing Platform/task/src/platform/businesslayer/services/SnippetService.java
+++ Code Sharing Platform/Code Sharing Platform/task/src/platform/businesslayer/services/SnippetService.java
@@ -1,12 +1,13 @@
 package platform.businesslayer.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.servlet.ModelAndView;
 import platform.businesslayer.entities.Snippet;
 import platform.dataaccesslayer.SnippetsRepository;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
@@ -14,7 +15,7 @@ import java.time.format.DateTimeFormatter;
 public class SnippetService {
     private final SnippetsRepository snippetsRepository;
     private static final String DATE_FORMATTER = "yyyy/MM/dd HH:mm:ss";
-    private Snippet snippetDB =new Snippet("public static void ...","2020/01/01 12:00:03");
+    private Snippet snippetDB = new Snippet("public static void ...", "2020/01/01 12:00:03");
 
     @Autowired
     public SnippetService(SnippetsRepository snippetsRepository) {
@@ -32,49 +33,17 @@ public class SnippetService {
         return ResponseEntity.ok().body("{}");
     }
 
-    public ResponseEntity<String> usingResponseEntityBuilderAndHttpHeaders() {
-        HttpHeaders responseHeaders = new HttpHeaders();
-        responseHeaders.set("title", "Code");
-        String date = snippetDB.getDate();
-
-        return ResponseEntity.ok()
-                .headers(responseHeaders)
-                .body("<html>\n" +
-                        "   <head>\n" +
-                        "       <title>Code</title>\n" +
-                        "   </head>\n" +
-                        "   <body>\n" +
-                        "       <span id=\"load_date\">\n" +
-
-                                     date +
-
-                                "</span>" +
-                        "      <pre id=\"code_snippet\">\n" +
-
-                                    snippetDB.getCode() +
-
-                                "</pre>\n" +
-                            "</body>\n" +
-                        "</html>");
+    public ModelAndView getCode(HttpServletResponse response) {
+        response.addHeader("Content-Type", "text/html");
+        ModelAndView model = new ModelAndView("Code");
+        model.addObject("code", snippetDB.getCode());
+        model.addObject("date", snippetDB.getDate());
+        return model;
     }
 
-    public ResponseEntity<String> codeNew() {
-        HttpHeaders responseHeaders = new HttpHeaders();
-        responseHeaders.set("title", "Create");
-
-        return ResponseEntity.ok()
-                .headers(responseHeaders)
-                .body("<html>\n" +
-                        "<head>\n" +
-                        "<title>Create</title>\n" +
-                        "</head>\n" +
-                        "<body>\n" +
-                        "<textarea id=\"code_snippet\">\n" +
-                        "// write your code here" +
-                        "</textarea>\n" +
-                        "<button id=\"send_snippet\" type=\"submit\" onclick=\"send()\">Submit</button\">\n" +
-                        "</body>\n" +
-                        "</html>");
+    public ModelAndView getSubmitCode(HttpServletResponse response) {
+        response.addHeader("Content-Type", "text/html");
+        return new ModelAndView("CodeNew");
     }
 
 }
--- Code Sharing Platform/Code Sharing Platform/task/src/platform/presentation/ApiController.java
+++ Code Sharing Platform/Code Sharing Platform/task/src/platform/presentation/ApiController.java
@@ -6,8 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 import platform.businesslayer.entities.Snippet;
 import platform.businesslayer.services.SnippetService;
+
+import javax.servlet.http.HttpServletResponse;
 
 @RestController
 public class ApiController {
@@ -28,12 +31,12 @@ public class ApiController {
 
 
     @GetMapping("/code")
-    public ResponseEntity<String> usingResponseEntityBuilderAndHttpHeaders() {
-        return snippetService.usingResponseEntityBuilderAndHttpHeaders();
+    public ModelAndView getSnippet(HttpServletResponse response) {
+        return snippetService.getCode(response);
     }
 
     @GetMapping("/code/new")
-    public ResponseEntity<String> codeNew() {
-        return snippetService.codeNew();
+    public ModelAndView submitCode(HttpServletResponse response) {
+        return snippetService.getSubmitCode(response);
     }
 }
