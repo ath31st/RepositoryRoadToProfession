41d500531dd269bfcaa5e69442ae0777c57d864f
Vladimir Olennikov <ath31st@yandex.ru>
2022-05-17 12:21:19 +0300

    Project "Text editor", stage 4/4 v1

--- Text Editor/Text Editor/task/src/editor/TextEditor.java
+++ Text Editor/Text Editor/task/src/editor/TextEditor.java
@@ -6,80 +6,99 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 public class TextEditor extends JFrame {
+    JTextField searchField = new JTextField();
+    JTextArea textArea = new JTextArea();
+    JButton saveButton = new JButton("Save");
+    JButton openButton = new JButton("Open");
+    JButton searchButton = new JButton("Search");
+    JButton previousMatchButton = new JButton("Previous");
+    JButton nextMatchButton = new JButton("Next");
+    JScrollPane scrollPane = new JScrollPane();
+    JMenuBar menuBar = new JMenuBar();
+    JMenu fileMenu = new JMenu("File");
+    JMenu searchMenu = new JMenu("Search");
+    JMenuItem openItem = new JMenuItem("Open");
+    JMenuItem saveItem = new JMenuItem("Save");
+    JMenuItem exitItem = new JMenuItem("Exit");
+    JMenuItem menuStartSearchItem = new JMenuItem("Search");
+    JMenuItem menuPreviousMatchItem = new JMenuItem("Previous");
+    JMenuItem menuNextMatchItem = new JMenuItem("Next");
+    JMenuItem menuUseRegExpItem = new JMenuItem("Use regular expressions");
+    JFileChooser jFileChooser = new JFileChooser();
+    JCheckBox regExpCheckBox = new JCheckBox();
+
+
     public TextEditor() {
         super("Text Editor");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(600, 400);
+        setSize(900, 600);
         setLocationRelativeTo(null);
         setVisible(true);
 
-        JTextField fileNameField = new JTextField();
-        fileNameField.setName("FilenameField");
-        fileNameField.setBounds(10, 20, 200, 30);
-        add(fileNameField);
+        regExpCheckBox.setName("UseRegExCheckbox");
+        regExpCheckBox.setText("Use regex");
+        regExpCheckBox.setBounds(530,25,100,20);
+        add(regExpCheckBox);
 
-        JTextArea textArea = new JTextArea();
+        searchField.setName("SearchField");
+        searchField.setBounds(10, 20, 200, 30);
+        add(searchField);
+
         textArea.setName("TextArea");
-        textArea.setBounds(10, 100, 550, 200);
+        textArea.setBounds(10, 100, 750, 400);
         add(textArea);
 
-        JButton saveButton = new JButton("Save");
+        searchButton.setName("StartSearchButton");
+        searchButton.setBounds(240, 20, 80, 30);
+        add(searchButton);
+
+        previousMatchButton.setName("PreviousMatchButton");
+        previousMatchButton.setBounds(340, 20, 80, 30);
+        add(previousMatchButton);
+
+        nextMatchButton.setName("NextMatchButton");
+        nextMatchButton.setBounds(440, 20, 80, 30);
+        add(nextMatchButton);
+
         saveButton.setName("SaveButton");
-        saveButton.setBounds(230, 20, 100, 30);
+        saveButton.setBounds(730, 20, 80, 30);
         add(saveButton);
 
-        saveButton.addActionListener(e -> {
-            String fileName = fileNameField.getText();
-            String text = textArea.getText();
-            try {
-                Files.write(Paths.get(fileName), text.getBytes());
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-        });
+        openButton.setName("OpenButton");
+        openButton.setBounds(640, 20, 80, 30);
+        add(openButton);
 
-        JButton loadButton = new JButton("Load");
-        loadButton.setName("LoadButton");
-        loadButton.setBounds(340, 20, 100, 30);
-        add(loadButton);
-
-        loadButton.addActionListener(e -> {
-            textArea.setText(null);
-            String textFromFile;
-            try {
-                textFromFile = Files.readString(Paths.get(fileNameField.getText()));
-            } catch (IOException ex) {
-                throw new RuntimeException(ex);
-            }
-            textArea.setText(textFromFile);
-        });
-
-        JScrollPane scrollPane = new JScrollPane();
         scrollPane.setName("ScrollPane");
         add(scrollPane);
 
-
-        JMenuBar menuBar = new JMenuBar();
         setJMenuBar(menuBar);
 
-        JMenu fileMenu = new JMenu("File");
         fileMenu.setName("MenuFile");
-       // fileMenu.setMnemonic(KeyEvent.VK_F);
         menuBar.add(fileMenu);
 
-        JMenuItem loadItem = new JMenuItem("Load");
-        loadItem.setName("MenuLoad");
-        JMenuItem saveItem = new JMenuItem("Save");
+        openItem.setName("MenuOpen");
         saveItem.setName("MenuSave");
-        JMenuItem exitItem = new JMenuItem("Exit");
         exitItem.setName("MenuExit");
-        fileMenu.add(loadItem);
+        fileMenu.add(openItem);
         fileMenu.add(saveItem);
         fileMenu.addSeparator();
         fileMenu.add(exitItem);
 
+        searchMenu.setName("MenuSearch");
+        menuBar.add(searchMenu);
+
+        menuStartSearchItem.setName("MenuStartSearch");
+        menuPreviousMatchItem.setName("MenuPreviousMatch");
+        menuNextMatchItem.setName("MenuNextMatch");
+        menuUseRegExpItem.setName("MenuUseRegExp");
+        searchMenu.add(menuStartSearchItem);
+        searchMenu.add(menuPreviousMatchItem);
+        searchMenu.add(menuNextMatchItem);
+        searchMenu.add(menuUseRegExpItem);
+
+
         saveItem.addActionListener(e -> {
-            String fileName = fileNameField.getText();
+            String fileName = searchField.getText();
             String text = textArea.getText();
             try {
                 Files.write(Paths.get(fileName), text.getBytes());
@@ -88,16 +107,37 @@ public class TextEditor extends JFrame {
             }
         });
 
-        loadItem.addActionListener(e -> {
+        openItem.addActionListener(e -> {
             textArea.setText(null);
             String textFromFile;
             try {
-                textFromFile = Files.readString(Paths.get(fileNameField.getText()));
+                textFromFile = Files.readString(Paths.get(searchField.getText()));
             } catch (IOException ex) {
                 throw new RuntimeException(ex);
             }
             textArea.setText(textFromFile);
         });
         exitItem.addActionListener(e -> dispose());
+
+        openButton.addActionListener(e -> {
+            textArea.setText(null);
+            String textFromFile;
+            try {
+                textFromFile = Files.readString(Paths.get(searchField.getText()));
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+            textArea.setText(textFromFile);
+        });
+
+        saveButton.addActionListener(e -> {
+            String fileName = searchField.getText();
+            String text = textArea.getText();
+            try {
+                Files.write(Paths.get(fileName), text.getBytes());
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        });
     }
 }
